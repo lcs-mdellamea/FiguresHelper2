3f6682a78d48785f998464bc0d7d16c1d1ccbd47
Madison Dellamea <mdellamea@lcs.on.ca>
2021-12-02 10:04:05 -0500

    Initial Commit

---
+++ FiguresHelper/FiguresHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiguresHelperApp.swift
+//  FiguresHelper
+//
+//  Created by Madison Dellamea on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct FiguresHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
